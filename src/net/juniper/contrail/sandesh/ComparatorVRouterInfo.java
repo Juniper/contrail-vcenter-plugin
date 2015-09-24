0a1c154abac5d8df0bd5da90d4678714c427e286
Andra Cismaru <andrac@juniper.net>
2015-09-24 11:29:08 -0700

    Fixed ordering of IP Addresses

--- src/net/juniper/contrail/sandesh/ComparatorVRouterInfo.java
+++ src/net/juniper/contrail/sandesh/ComparatorVRouterInfo.java
@@ -2,14 +2,14 @@ package net.juniper.contrail.sandesh;
 
 import java.util.Comparator;
 
-public class ComparatorVRouterInfo 
+public class ComparatorVRouterInfo
     implements Comparator<VRouterInfo> {
-    
+
     public int compare(VRouterInfo vr1, VRouterInfo vr2) {
-        int cmp1 = vr1.getIpAddr().compareTo(vr2.getIpAddr());
-        if (cmp1 != 0) {
-            return cmp1;
-        }
+        int cmp = IpAddressUtil.compare(vr1.getIpAddr(), vr2.getIpAddr());
+        if (cmp != 0) {
+            return cmp;
+        }     
         return vr1.getEsxiHost().compareTo(vr2.getEsxiHost());
     }
 }
