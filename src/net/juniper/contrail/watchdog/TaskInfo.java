c967cfc595a66c647ba0634c620fa0831859c2ff
Andra Cismaru <andrac@juniper.net>
2015-10-07 14:41:41 -0700

    Added Http Introspect for WatchDog at http://<controller>:8777/Snh_TaskWatchDog

---
+++ src/net/juniper/contrail/watchdog/TaskInfo.java
@@ -0,0 +1,128 @@
+/* This class will be generated based on struct TaskInfo
+ * from vcenter.sandesh
+ */
+package net.juniper.contrail.watchdog;
+
+import net.juniper.contrail.sandesh.DetailLevel;
+import net.juniper.contrail.sandesh.SandeshObject;
+import net.juniper.contrail.sandesh.SandeshObjectList;
+
+public class TaskInfo implements SandeshObject
+{
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    private void writeFieldName(StringBuilder s, int identifier,
+            DetailLevel detail)
+    {
+        s.append("<name type=\"string\" identifier=\"")
+        .append(identifier)
+        .append("\"");
+        s.append(">")
+        .append(name)
+        .append("</name>");
+    }
+
+    private boolean blocked;
+
+    public boolean getBlocked() {
+        return blocked;
+    }
+
+    public void setBlocked(boolean blocked) {
+        this.blocked = blocked;
+    }
+
+    private void writeFieldBlocked(StringBuilder s, int identifier,
+            DetailLevel detail)
+    {
+        s.append("<blocked type=\"bool\" identifier=\"")
+        .append(identifier)
+        .append("\">")
+        .append(blocked)
+        .append("</blocked>");
+    }
+
+    private String timeStamp;
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+    private void writeFieldTimeStamp(StringBuilder s, int identifier,
+            DetailLevel detail)
+    {
+        s.append("<timeStamp type=\"string\" identifier=\"")
+        .append(identifier)
+        .append("\"");
+        s.append(">")
+        .append(timeStamp)
+        .append("</timeStamp>");
+    }
+
+    private String timeout;
+    public String getTimeout() {
+        return timeout;
+    }
+    public void setTimeout(String timeout) {
+        this.timeout = timeout;
+    }
+    private void writeFieldTimeout(StringBuilder s, int identifier,
+            DetailLevel detail)
+    {
+        s.append("<timeout type=\"string\" identifier=\"")
+        .append(identifier)
+        .append("\"");
+        s.append(">")
+        .append(timeout)
+        .append("</timeout>");
+    }
+
+    private SandeshObjectList<String> stackTrace;
+
+    public SandeshObjectList<String> getStackTrace() {
+        return stackTrace;
+    }
+
+    public void setStackTrace(SandeshObjectList<String> stackTrace) {
+        this.stackTrace = stackTrace;
+    }
+
+    private void writeFieldStackTrace(StringBuilder s, int identifier,
+            DetailLevel detail)
+    {
+        stackTrace.writeObject(s, "StackTrace", detail, identifier);
+    }
+
+    public TaskInfo() {
+        this.stackTrace = new SandeshObjectList<String>(String.class);
+    }
+
+    @Override
+    public void writeObject(StringBuilder s,  DetailLevel detail, int identifier)
+    {
+        s.append("<TaskInfo type=\"struct\" identifier=\"")
+        .append(identifier)
+        .append("\">");
+        if (detail == DetailLevel.FULL) {
+            s.append("<TaskInfoStruct>");
+        }
+        int inner_id = 1;
+        writeFieldName(s, inner_id++, detail);
+        writeFieldBlocked(s, inner_id++, detail);
+        writeFieldTimeStamp(s, inner_id++, detail);
+        writeFieldTimeout(s, inner_id++, detail);
+        writeFieldStackTrace(s, inner_id++, detail);
+
+        if (detail == DetailLevel.FULL) {
+            s.append("</TaskInfoStruct>");
+        }
+        s.append("</TaskInfo>");
+    }
+}
