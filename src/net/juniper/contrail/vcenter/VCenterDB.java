e98eaf9fd2e9031730f802c2be3d9ab3ef884716
Amudha <amudha@juniper.net>
2018-01-18 08:41:41 -0800

    vcenter-plugin fixes
    -> Scan for VMI only if its a valid managed VM, don't scan through VMI
       for VMs that are ignored.
    -> Validate vrouterIpAddress when finding the vrouter IP from
       vrouterApiMap, log and return if vrouterIpAddress is null.
    
    Change-Id: Ia36f78f6005e1bffda63ffb0a777e0e1d185b437
    Partial-Bug: #1744120

--- src/net/juniper/contrail/vcenter/VCenterDB.java
+++ src/net/juniper/contrail/vcenter/VCenterDB.java
@@ -383,6 +383,9 @@ public class VCenterDB {
 
         if (vRouterIpAddress != null) {
             return vRouterIpAddress;
+        } else {
+            s_logger.debug(" vRouter IP mapping for Host: " + hostName +
+                    "does not exist");
         }
 
         VirtualMachine[] vms = host.getVms();
@@ -930,13 +933,13 @@ public class VCenterDB {
                     (VirtualMachine)vms[i], pTables[i],
                     host, vrouterIpAddress);
 
-            readVirtualMachineInterfaces(vmInfo);
-
             // Ignore virtual machine?
             if (vmInfo.ignore()) {
                 s_logger.debug(" Ignoring vm: " + vmInfo.getName());
                 continue;
             }
+
+            readVirtualMachineInterfaces(vmInfo);
 
             s_logger.info("Read from vcenter " + vmInfo);
 
